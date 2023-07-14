8ea2b86c25f4d82d73bc4a65786905d95efa5195
Mahdi Khan Beigi <mahdi.khanbeigi@yahoo.com>
2023-07-14 23:42:37 +0330

    deatch ThemeProvider from ElementProvider

--- src/App.tsx
+++ src/App.tsx
@@ -3,67 +3,71 @@ import App from "@app/containers/App";
 import { AuthProvider } from "@app/hook/auth";
 import { ThemeStyle } from "@app/containers/ThemeStyle";
 import { ElementsProvider } from "@front-app-react/elements";
+import { ThemeProvider } from "@front-app-react/theme";
 
 function MainApp() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <ElementsProvider
-          styles={{
-            btn: {
-              $bgColor: {
-                active: {
-                  name: "red",
-                },
-              },
-            },
-            overlayBg: {
-              name: "dark",
-              mood: 1,
-              opacity: 0.5,
-            },
-            dropdownMenu: {
-              style: {
-                $bgColor: {
-                  variant: {
-                    name: "light",
-                  },
-                },
-              },
-            },
-            modal: {
-              content: {
-                $bgColor: {
-                  variant: {
-                    name: "light",
-                  },
-                },
-                $borderColor: {
-                  variant: {
-                    name: "light",
-                    mood: 2,
-                  },
-                },
-                maxWidth: "500px",
-                borderRadius: "4px",
-              },
-            },
-            card: {
-              $bgColor: {
-                name: "yellow",
-              },
-            },
-          }}
-          theme={{
+        <ThemeProvider
+          {...{
             defaultColor: "light",
             defaultLang: "en-US",
             prefix: "app",
             storage: localStorage,
           }}
         >
-          <ThemeStyle />
-          <App />
-        </ElementsProvider>
+          <ElementsProvider
+            styles={{
+              btn: {
+                $bgColor: {
+                  active: {
+                    name: "red",
+                  },
+                },
+              },
+              overlayBg: {
+                name: "dark",
+                mood: 1,
+                opacity: 0.5,
+              },
+              dropdownMenu: {
+                style: {
+                  $bgColor: {
+                    variant: {
+                      name: "light",
+                    },
+                  },
+                },
+              },
+              modal: {
+                content: {
+                  $bgColor: {
+                    variant: {
+                      name: "light",
+                    },
+                  },
+                  $borderColor: {
+                    variant: {
+                      name: "light",
+                      mood: 2,
+                    },
+                  },
+                  maxWidth: "500px",
+                  borderRadius: "4px",
+                },
+              },
+              card: {
+                $bgColor: {
+                  name: "yellow",
+                },
+              },
+            }}
+          >
+            <ThemeStyle />
+            <App />
+          </ElementsProvider>
+        </ThemeProvider>
       </BrowserRouter>
     </AuthProvider>
   );
